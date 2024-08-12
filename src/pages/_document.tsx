c9d96f436885f3cb9d6ecfabc1fa8e89dd371547
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-08-12 14:04:41 +0100

    Fix page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <title>SSV Rewards</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
