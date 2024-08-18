9669ed358c9c5150be60879c651d599352a39e04
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-08-18 18:05:15 +0100

    Add logos to tabs

--- src/components/RewardProgramTabs.tsx
+++ src/components/RewardProgramTabs.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { VStack, Text, Tabs, TabList, Tab, TabPanels, TabPanel, Spinner } from "@chakra-ui/react"
+import { VStack, HStack, Text, Tabs, TabList, Tab, TabPanels, TabPanel, Image } from "@chakra-ui/react"
 
 import IncentivesMainnetTab from "./IncentivesMainnetTab"
 
@@ -14,15 +14,17 @@ export default function RewardsContainer({ address, customRpc }) {
         <VStack gap={6} width="100%">
             <Tabs isFitted width="100%" variant={"RewardsTabs"} onChange={handleTabChange}>
                 <TabList borderColor={"transparent"} mb={0}>
-                    <Tab py={2} fontWeight={"semibold"} fontSize={"lg"}>
-                        <Text mb={activeTabIndex == 0 ? "4px" : "0"} mr={activeTabIndex == 0 ? "4px" : "0"}>
-                            Mainnet Incentives
-                        </Text>
+                    <Tab py={2} fontWeight={"semibold"} fontSize={"lg"} gap={3}>
+                        <HStack mb={activeTabIndex == 0 ? "4px" : "0"} mr={activeTabIndex == 0 ? "4px" : "0"}>
+                            <Image maxW={4} objectFit={"cover"} src={"./images/SSVLogo.svg"} alt={"SSV Logo"} />
+                            <Text>SSV Mainnet Incentives</Text>
+                        </HStack>
                     </Tab>
-                    <Tab py={2} fontWeight={"semibold"} fontSize={"lg"}>
-                        <Text mb={activeTabIndex == 1 ? "4px" : "0"} ml={activeTabIndex == 1 ? "4px" : "0"}>
-                            Lido Vault (Coming soon…)
-                        </Text>
+                    <Tab py={2} fontWeight={"semibold"} fontSize={"lg"} gap={3}>
+                        <HStack mb={activeTabIndex == 1 ? "4px" : "0"} ml={activeTabIndex == 1 ? "4px" : "0"}>
+                            <Image maxW={6} borderRadius={"full"} objectFit={"cover"} src={"./images/LidoLogo.png"} alt={"Lido Logo"} />
+                            <Text>Lido Vault (Coming soon)</Text>
+                        </HStack>
                     </Tab>
                 </TabList>
                 <TabPanels className="contentContainer" border={"2px solid"} borderTop={"none"} borderBottomRadius={"20px"}>
