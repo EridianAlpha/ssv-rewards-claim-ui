7444b815ed8a6db54b5b970e0016b455b971134f
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-03-12 23:03:36 +0000

    Update footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,13 @@
-import { Flex, Link, Text, Box, Code } from "@chakra-ui/react"
+import { Flex, Link, Text } from "@chakra-ui/react"
 
 import NextLink from "next/link"
-import { borderRadius } from "polished"
 
 export default function Footer() {
     return (
         <Flex direction={"column"} alignItems={"center"} pb={5} px={3} gap={1}>
             <Text fontWeight={"semibold"}>
                 Built with ❤️ by{" "}
-                <Link as={NextLink} href={"https://eridianalpha.com"} color={"blue"} textDecoration={"underline"} target="_blank">
+                <Link as={NextLink} href={"https://eridian.xyz"} color={"blue"} textDecoration={"underline"} target="_blank">
                     Eridian
                 </Link>
             </Text>
