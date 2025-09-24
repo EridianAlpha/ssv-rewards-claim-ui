30697eef6191c23c334ae706d568df7490763034
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-09-24 18:45:47 +0100

    Implement scraper blocker

--- src/pages/api/list-files.ts
+++ src/pages/api/list-files.ts
@@ -3,9 +3,22 @@ import fs from "fs"
 import path from "path"
 
 export default function handler(req, res) {
-    const dataDirectory = path.join(process.cwd(), "public", "data", req.query.directory)
-    const files = fs.readdirSync(dataDirectory)
-    const jsonFiles = files.filter((file) => file.startsWith("MerkleProof-") && file.endsWith(".json"))
+    const ua = req.headers["user-agent"]?.toLowerCase() || "unknown"
 
-    res.status(200).json(jsonFiles)
+    // Block python scrapers
+    if (ua.includes("python")) {
+        console.log("❌ Blocked Python scraper")
+        return res.status(403).json({ error: "Forbidden" })
+    }
+
+    try {
+        const dataDirectory = path.join(process.cwd(), "public", "data", req.query.directory)
+        const files = fs.readdirSync(dataDirectory)
+        const jsonFiles = files.filter((file) => file.startsWith("MerkleProof-") && file.endsWith(".json"))
+
+        res.status(200).json(jsonFiles)
+    } catch (err) {
+        console.error("❌ Error reading directory:", err)
+        res.status(500).json({ error: "Internal server error" })
+    }
 }
